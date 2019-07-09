84f8f516b8d3890910b06066d0d0ef3186e4ce83
wuhuaji0 <wuhuaji0@gmail.com>
2019-07-09 23:27:28 +0800

    初步改造为支持管道的版本

--- main.c
+++ main.c
@@ -61,23 +61,55 @@ char *readline(){
 }
 
 
+/**
+ * 如果这一行命令，是包含管道，拆分成多个命令组
+ * @return
+ */
+char **splite_pipe(char *line){
+    char delimiter[] = "|";
+    int max_commond = 10;
+    char **commands = malloc(sizeof(char *) * max_commond);
+
+    if( commands == NULL ){
+        printf("malloc 分配空间失败\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *item = strtok(line,delimiter);
+    int index = 0;
+    while (item != NULL){
+        commands[index] = item;
+        item = strtok(NULL,delimiter);
+        index++;
+        //如果超出max_argv - 1个字符，重新分配空间；为什么需要 - 1 ，因为要给最后的NULL留出空间
+        if( index >= max_commond - 1 ){
+            max_commond += max_commond;
+            commands = realloc(commands,max_commond);
+        }
+    }
+    commands[index] = NULL;
+
+//    for (int i = 0; i < index; ++i) {
+//        printf("输入的第%d个参数：%s\n",i,commands[i]);
+//    }
+    return commands;
+}
+
 //解析读取到的字符串
-char **spite_argv(char *line){
-    char delemite[] = " \t\r\n";
+char **splite_argv(char *line){
+    char delimiter[] = " \t\r\n";
     int max_argv = 100;
     char **argv = malloc(sizeof(char *) * max_argv);
-
-
     if( argv == NULL ){
         printf("malloc 分配空间失败\n");
         exit(EXIT_FAILURE);
     }
-    char *item = strtok(line,delemite);
+    char *item = strtok(line,delimiter);
 
     int index = 0;
     while (item != NULL){
         argv[index] = item;
-        item = strtok(NULL,delemite);
+        item = strtok(NULL,delimiter);
         index++;
 
         //如果超出max_argv - 1个字符，重新分配空间；为什么需要 - 1 ，因为要给最后的NULL留出空间
@@ -87,51 +119,118 @@ char **spite_argv(char *line){
         }
     }
     argv[index] = NULL;
+
+//    for (int i = 0; i < index; ++i) {
+//        printf("输入的第%d个参数：%s\n",i,argv[i]);
+//    }
     return argv;
 }
 
 
+/**
+ * 计算写入了多少个命令
+ * @return
+ */
+int count_command(char **argv){
+    int count = 0 ;
+    while (argv[count] != NULL){
+        count++;
+    }
+    return count;
+}
 
-int run(char **argv){
-    if( argv[0] == NULL ){
+
+int run(char **commands){
+    int count = count_command(commands);
+    if( count == 0 ){
         prompt();
         return 0;
     }
 
-    //判断是否是内建命令
-    for (int i = 0; i < sizeof(builtins) / sizeof(char *); ++i) {
-        if( strcmp(builtins[i],argv[0]) == 0){
-            (*builtin_funcs[i])(argv);
-            prompt();
-            return 0;
-        }
-    }
+//判断是否是内建命令
+//    for (int i = 0; i < sizeof(builtins) / sizeof(char *); ++i) {
+//        if( strcmp(builtins[i],argv[0]) == 0){
+//            (*builtin_funcs[i])(argv);
+//            prompt();
+//            return 0;
+//        }
+//    }
 
-    int pid = fork();
     int status;
-    if( pid == 0 ){
-        int result = execvp(argv[0],argv);
-        if( result == -1 ){
-            perror("shell");
-            exit(EXIT_FAILURE);
+    int ppid = getpid();
+    int count_fd = (count - 1) * 2;
+    int fd[count_fd]; // 管道的文件描述符
+    for (int i = 0; i < count; ++i) {
+
+        if( count > 1 && i < count - 1){
+            pipe(&fd[i * 2]);
         }
-    }else{
-        wait(&status);
-        prompt();
+
+        int pid = fork();
+
+        if( pid == 0 ){
+            if( count > 1 ){
+                if(i == 0 ){
+                    dup2(fd[1],STDOUT_FILENO);
+                    //关闭不需要的描述符
+                    close(fd[0]);
+                }else if (i < count - 1){
+                    dup2(fd[(i - 1) * 2 ],STDIN_FILENO);
+                    dup2(fd[i * 2 + 1],STDOUT_FILENO);
+
+                    for (int j = 0; j < count_fd ; ++j) {
+                        if( j == (i - 1) * 2 || j == i * 2 + 1 ){
+                            continue;
+                        }
+                        close(fd[j]);
+                    }
+                }else if(i == count - 1 ){
+                    dup2(fd[(i - 1) * 2],STDIN_FILENO);
+                    for (int j = 0; j < count_fd; ++j) {
+                        if( j == (i - 1) * 2){
+                            continue;
+                        }
+                        printf("j : %d\n",j);
+                        close(fd[j]);
+                    }
+                }
+            }
+
+            char **argv = splite_argv(commands[i]);
+            int result = execvp(argv[0],argv);
+            free(argv);
+
+            if( result == -1 ){
+                perror("shell");
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_SUCCESS);
+            break;
+        }
     }
 
+    //关闭父进程所有的文件描述符
+    for (int j = 0; j < count_fd; ++j) {
+        printf("father j : %d\n",j);
+        close(fd[j]);
+    }
+    for (int k = 0; k < count; ++k) {
+        wait(NULL);
+    }
+
+    prompt();
     return 0;
+
 }
 
 void loop(){
     prompt();
     while (1){
         char *line = readline();
-        char **argv = spite_argv(line);
-        run(argv);
+        char **commands =  splite_pipe(line);
+        run(commands);
 
-        free(line);
-        free(argv);
+        free(commands);
     }
 }
 
