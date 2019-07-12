fa1499d90fd102dd474bb9d502c8b5c20826b932
wuhuaji0 <wuhuaji0@gmail.com>
2019-07-12 23:33:21 +0800

    优化 history_record 函数，使用标准io库来记录history

--- src/history.c
+++ src/history.c
@@ -7,37 +7,21 @@
 #include <unistd.h>
 #include <string.h>
 
-static int need_newline(fd){
-    //如果读出不为空，则换行之后再写入
-    char buffer_read[10];
-    int read_count = read(fd,buffer_read,10);
-    if( read_count > 0 ){
-        if( write(fd,"\n",1) == -1 ){
-            perror("write error"); return -1;
-        }
-    }
-    return 0 ;
-}
-
 //记录命令输入日志
 int history_record(char *commands){
-    int fd = open(".tinyshell_history",O_RDWR | O_APPEND | O_CREAT, 0755);
-    if( fd == -1 ){ //如果文件不存在
-        perror("tinyshell_history");
-        return -1;
+    FILE *file = fopen(".tinyshell_history","ar+");
+    if( file == NULL ){
+        perror("读取history文件失败");
+        return 0;
     }
-
-    //判断是否需要换行再写入
-    need_newline(fd);
-
-    int commonds_length = strlen(commands);
-    int write_result = write(fd,commands, strlen(commands));
-    if( write_result == -1 ){
-        perror("write error");
+    //只要不是在文件开头，先换行
+    if( ftell(file) != 0 ){
+        fputc('\n',file);
     }
-
-    close(fd);
-    return 0;
+    int result = fputs(commands,file);
+    printf("写入结果: %d\n",result);
+    fclose(file);
+    return 0 ;
 }
 
 
